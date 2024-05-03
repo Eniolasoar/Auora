3e0b35417f00d31ea7b14784795facaa2e96c2c9
Eniolasoar <eniolanishe@gmail.com>
2024-05-03 13:11:42 +0100

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl text-green-500">AUORA!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
