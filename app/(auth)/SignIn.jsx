6e8c34ca06c159ddbb8dd9f29d941db8e4581c1c
Eniolasoar <eniolanishe@gmail.com>
2024-05-07 19:37:29 +0100

    third

--- app/(auth)/SignIn.jsx
+++ app/(auth)/SignIn.jsx
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
-const SignIn = () => {
+import { View, Text, Image } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native';
+import { images } from '../../constants';
+import FormField from '../../components/FormField';
+import CustomButton from '../../components/CustomButton';
+const Sign = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  });
+  const submit=()=>{}
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full ">
+     <ScrollView className="mx-4">
+      <View classsName="w-full justify-center h-full px-10 my-20 items-center ">
+         <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]pt-10 "/>
+         <Text className="text-2xl text-white font-semibold mt-4 text-semibold ">Log in to Auora</Text>
+         <FormField
+         title="Email"
+         value={form.email}
+         handleChangeText={(e)=>setForm({...form,email:e})}
+         otherStyles="mt-7"
+         keyboardType="email-address"
+         />
+         <FormField
+         title="Password"
+         value={form.password}
+         handleChangeText={(e)=>setForm({...form,password:e})}
+         otherStyles="mt-7"
+         keyboardType="email-address"
+         />
+         <CustomButton title="Sign In" handlePress={submit} containerStyles="mt-7"/>
+      </View>
+     </ScrollView>
+    </SafeAreaView>
   )
 }
 
-export default SignIn
+export default Sign;
--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { ScrollView, Text, View ,Image} from 'react-native';
+import { Link,Redirect,router } from 'expo-router';
 import {SafeAreaView} from "react-native-safe-area-context";
 import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 export default function App() {
   
   return (
    
    <SafeAreaView className="bg-primary h-full">
     <ScrollView contentContainerStyle={{ height:"100%" }}>
-      <View className="w-full justify-center items-center h-full px-4 "></View>
+      <View className="w-full justify-center items-center min-h-[85vh] px-4 ">
+        <Image source={images.logo} resizeMode="contain" className="w-[130px] h-[84px]" />
+        <Image source={images.cards} className="max-w--[380px] w-full h-[300px]" resizeMode="contain"/>
+        <View className="relative mt-5">
+            <Text className="text-3xl text-white text-center font-bold">Discover Endless Possiblities with <Text className="text-secondary-200">Auora</Text></Text>
+            <Image source={images.path} className="w-[36px] h-[15px] absolute -bottom-2 -right-0.5" resizeMode='contain' />
+        </View>
+
+        <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation:
+          embark on a journey of limitless exploration with Aoura
+        </Text>
+
+        <CustomButton title="Continue With Email" handlePress={()=>router.push("/SignIn")} containerStyles="w-full mt-7"/>
+      </View>
     </ScrollView>
 
+    <StatusBar backgroundColor='red' style='dark'/>
    </SafeAreaView>
   );
 }
