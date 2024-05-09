f30c8e5873f314cde139e98660c11c7c9eeb07c0
Eniolasoar <eniolanishe@gmail.com>
2024-05-09 13:25:52 +0100

    SignUp and Sign Page added with Appwrite Config

--- app/(auth)/SignIn.jsx
+++ app/(auth)/SignIn.jsx
@@ -5,17 +5,21 @@ import { ScrollView } from 'react-native';
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton';
-const Sign = () => {
+import { Link } from 'expo-router';
+const SignIn = () => {
   const [form, setForm] = useState({
     email: '',
     password: ''
   });
+
+  const [isSubmitting,setIsSubmitting]=useState(false);
+  
   const submit=()=>{}
   return (
     <SafeAreaView className="bg-primary h-full ">
-     <ScrollView className="mx-4">
-      <View classsName="w-full justify-center h-full px-10 my-20 items-center ">
-         <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px]pt-10 "/>
+     <ScrollView >
+      <View className="w-full justify-center min-h-[83vh] px-10 my-6 ">
+         <Image source={images.logo} resizeMode='contain' className="w-[115px] h-[35px] "/>
          <Text className="text-2xl text-white font-semibold mt-4 text-semibold ">Log in to Auora</Text>
          <FormField
          title="Email"
@@ -29,13 +33,19 @@ const Sign = () => {
          value={form.password}
          handleChangeText={(e)=>setForm({...form,password:e})}
          otherStyles="mt-7"
-         keyboardType="email-address"
+         keyboardType="password"
          />
-         <CustomButton title="Sign In" handlePress={submit} containerStyles="mt-7"/>
+         <CustomButton title="Sign In" handlePress={submit} containerStyles="mt-7" isLoading={isSubmitting}/>
+         <View className="justify-center pt-5 flex-row gap-2">
+          <Text className="text-lg text-gray-100 font-pregular">
+            Don't have an account?
+          </Text>
+          <Link href="/SignUp" className='text-lg font-psemibold text-secondary'>SignUp</Link>
+         </View>
       </View>
      </ScrollView>
     </SafeAreaView>
   )
 }
 
-export default Sign;
+export default SignIn;
